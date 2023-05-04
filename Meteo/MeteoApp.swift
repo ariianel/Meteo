298eeb274feac3c9033f9e0cc218b17367035632
ariianel <lucariane03@gmail.com>
2023-05-05 00:36:56 +0200

    Initial Commit

---
+++ Meteo/MeteoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeteoApp.swift
+//  Meteo
+//
+//  Created by Ariane Luc on 05/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MeteoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
